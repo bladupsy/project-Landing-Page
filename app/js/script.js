270ac0a26d160e64e76c7450eb1a8e6796f1d46a
bladupsy <gisela.cristaldo11@gmail.com>
2021-02-03 13:07:10 -0300

    Parte 1

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+/*const hamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('Click', function(){
+  console.log('open hamburger');
+
+
+  if(btnHamburger.classList.contains('open')) {
+    
+      btnHamburger.classList.add('open');
+
+
+  }else {
+    btnHamburger.classList.add('open');
+  }
+})*/
+
+/*
+$(document).ready(function(){
+	$('#btnHamburger').click(function(){
+		$(this).toggleClass('open');
+	});
+});*/
